54f076b12e74e2bea1ad131a984813fcb2fc80b7
Kamon <harrellkamon@gmail.com>
2016-11-12 16:38:24 +1300

    added blog title

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -5,6 +5,7 @@ const Logo = props => {
     <div className="logo">
       <img src="images/profile-placeholder.gif" alt="this is a tree eventually" />
       <span className="Details">{props.me}<br />{props.github}<br />{props.linkedin}</span>
+      <h1> KAMONS BLOG</h1>
     </div>
   )
 }
