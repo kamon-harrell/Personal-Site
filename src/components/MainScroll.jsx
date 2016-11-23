3afb1a37809563cccd2bcad50e2d82a083ea1bc8
Kamon <harrellkamon@gmail.com>
2016-11-23 19:09:18 +1300

    trying to build list example as component,no luck so far

--- src/components/MainScroll.jsx
+++ src/components/MainScroll.jsx
@@ -2,13 +2,22 @@ import React from 'react'
 import Infinite from 'react-infinite'
 import { Link } from 'react-router'
 
+
+
 export default React.createClass({
   render () {
     return (
-      <Infinite containerHeight={200} elementHeight={[111, 252, 143]}>
-          <div className="111-px"><h1> DIV 1</h1></div>
-          <div className="252-px"><h1> DIV 2</h1></div>
-          <div className="143-px"><h1> DIV 3</h1></div>
+
+      <Infinite containerHeight={200} elementHeight={40}>
+          <div className="one"><h1>div</h1></div>
+          <div className="two"><h1>div</h1></div>
+          <div className="three"><h1>div</h1></div>
+            <div className="three"><h1>div</h1></div>
+              <div className="three"><h1>div</h1></div>
+                <div className="three"><h1>div</h1></div>
+                  <div className="three"><h1>div</h1></div>
+                    <div className="three"><h1>div</h1></div>
+          
       </Infinite>
     )
   }
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,14 +1,68 @@
 import React from 'react'
+import Infinite from 'react-infinite'
+import { Link } from 'react-router'
 
-export default React.createClass({
-  render () {
-    const { title } = this.props
+const Post = props => {
+  return (
+    const ListItem = React.createClass({
+      render () => {
+        return <div className="infin-list-item">
+          Blog Post {this.props.num}
+        </div>
+      }
+    })
 
-    return (
-      <div className="Post">
-        <h2>{title}</h2>
-        <p>Lorem ipsum random words</p>
-      </div>
-    )
-  }
-})
+    const InfiniteList = React.createClass({
+      getInitialState () => {
+        return {
+          elements: this.buildElements( 0, 20 ),
+          isInfiniteLoading: false
+        }
+      },
+
+      buildElements (start, end) => {
+        const elements = []
+        for (var i = start; i < end; i++) {
+          elements.push(<ListItem key={i} num={i}/>)
+        }
+        return elements
+      },
+
+      handleInfiniteLoad () => {
+        let that = this
+        this.setState({
+          isInfiniteLoading: true
+        })
+        setTimeout(() => {
+          const elemLength = that.state.elements.length,
+              newElements = that.buildElements(elemLength, elemLength + 1000)
+          that.setState({
+            isInfiniteLoading: false,
+            elements: that.state.elements.concat(newElements)
+          })
+        }, 1000)
+      },
+
+      elementInfiniteLoad () => {
+        return
+        <div className="infin-list-item">
+          Loading...
+        </div>
+      },
+
+      render () {
+        return <Infinite elementHeight={40}
+                         containerHeight={250}
+                         infiniteLoadBeginEdgeOffset={200}
+                         onInfiniteLoad={this.handleInfiniteLoad}
+                         loadingSpinnerDelegate={this.elementInfiniteLoad()}
+                         isInfiniteLoading={this.state.isInfiniteLoading}
+                         >
+            {this.state.elements}
+        </Infinite>;
+      }
+    })
+  )
+
+ReactDOM.render(<InfiniteList/>, document.getElementById('react-example-one'))
+export default Post
