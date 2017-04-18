6e0c5ee1041067a430c9eebe8d9407c35bcc53d6
Kamon <harrellkamon@gmail.com>
2017-04-18 17:18:02 -0600

    restarting

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
 
-import Header from './Header'
-
 export default React.createClass({
   render () {
     return (
       <div className='app-container'>
-        <Header />
         <h1>HELLO</h1>
       </div>
     )
--- src/index.js
+++ src/index.js
@@ -3,16 +3,12 @@ import ReactDOM from 'react-dom'
 import { Router, Route, IndexRoute, hashHistory } from 'react-router'
 
 import App from './components/App'
-import About from './components/About'
-import Projects from './components/Projects'
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     (
     <Router history={hashHistory}>
       <Route path='/' component={App} />
-        <Route path='/about' component={About} />
-        <Route path='/projects' component={Projects} />
     </Router>
     ),
     document.getElementById('app')
